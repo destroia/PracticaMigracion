ff39dce5fd982ceff2f81f3dc22c3278637a75f8
davidstiven123 <davidstiven123@gmail.com>
2024-03-11 16:03:18 -0500

    Agregar archivos de proyecto.

---
+++ Datas/DependencyContainerData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datas
+{
+    public static class DependencyContainerData
+    {
+        public static IServiceCollection AddData(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<PMContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("ConnectionMain")));
+
+            return services;
+        }
+    }
+}
---
+++ Datas/PMContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datas
+{
+    public class PMContext : DbContext
+    {
+        public PMContext(DbContextOptions<PMContext> options) : base(options)
+        {
+        }
+    }
+}
